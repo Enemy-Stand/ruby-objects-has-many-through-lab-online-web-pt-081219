2335e8539df209243a1e8bc74da2457da92705a7
Joseph Sesti <sestijoey63@gmail.com>
2019-09-20 19:52:55 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,4 @@
-#require 'pry'
+require 'pry'
 
 class Doctor
   attr_accessor :name
@@ -20,7 +20,7 @@ class Doctor
   
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
-    #binding.pry
+    binding.pry
   end
   
   def patients
